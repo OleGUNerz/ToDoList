8d6d6485040681606f9810c0da86985ea1dc28e7
OleGUNerz <48333829+OleGUNerz@users.noreply.github.com>
2022-06-30 00:13:04 +0200

    adaptation for mobile devices

--- js/main.js
+++ js/main.js
@@ -47,8 +47,8 @@ const createItems = (element, index) => {
     <div class="col-sm" id="job">${element.text}</div>
     <div class="col-sm" id="time">${element.data}</div>
     <div class="col-sm" id="free">${element.free ? 'свободен/на':'занят/та'}</div>
-    <button onclick ='delItem(${index})' style="width: auto;" class="btn btn-primary" display: inline; id="del_item">удалить</button>
-    <button onclick ='changeItem(${index})' style="width: auto;" class="btn btn-primary" display: inline; id="change_item">редактировать</button>
+    <button onclick ='delItem(${index})' style="width: auto;" class="btn btn-primary" display: inline; id="del_item">Удалить</button>
+    <button onclick ='changeItem(${index})' style="width: auto;" class="btn btn-primary" display: inline; id="change_item">Редактировать</button>
     <button onclick ='done(${index})' style="width: auto; background-color: rgb(2, 230, 2);" class="btn btn-primary" display: inline; id="done">Готово</button>
 </div>
 `
@@ -59,7 +59,7 @@ const createItemsDone = (element, index) => {
     <div class="col-sm" id="job">${element.text}</div>
     <div class="col-sm" id="time">${element.data}</div>
     <div class="col-sm" id="free">${element.free ? 'свободен/на':'занят/та'}</div>
-    <button onclick ='delItem(${index})' style="width: auto;" class="btn btn-primary" display: inline; id="del_item">удалить</button>
+    <button onclick ='delItem(${index})' style="width: auto;" class="btn btn-primary" display: inline; id="del_item">Удалить</button>
     <button onclick ='back(${index})' style="width: auto; background-color: rgb(2, 230, 153);" class="btn btn-primary" display: inline; id="back">Вернуть</button>
 </div>
 `
