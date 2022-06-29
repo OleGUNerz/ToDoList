f9d64cc1dbd7887e927cbd5b11f891ab57d4b056
OleGUNerz <48333829+OleGUNerz@users.noreply.github.com>
2022-06-29 13:10:08 +0200

    Added the done and return button.

--- js/main.js
+++ js/main.js
@@ -13,7 +13,11 @@ const jobCh = document.getElementById('jobCh');
 const timeCh = document.getElementById('timeCh');
 const saveCh = document.getElementById('saveCh');
 const snoCh = document.getElementById('noCh');
+const done_block = document.querySelector('.item-blockDone');
+const done_box = document.querySelector('.done_box');
 let storage;
+let storageDone =[];
+let storageSlicer;
 let chIt;
 let truCh;
 let moviCh;
@@ -21,8 +25,10 @@ let moviCh;
 const boxNone = ()=>{
     if(storage.length == 0){
         checkI.classList.add("none");
+        done_block.classList.add("none");
     }else{
         checkI.classList.remove("none");
+        done_block.classList.remove("none");
     }
 }
 !localStorage.items ? storage = [] : storage = JSON.parse(localStorage.getItem('items'));
@@ -32,25 +38,45 @@ function CreateItem(text, data, free){
     this.text = text;
     this.data = data;
     this.free = free;
+    this.done = false;
 };
 
 const createItems = (element, index) => {
     return `
-<div class="row" id="item">
+<div class="row ${element.done ? 'done' : ''}" id="item">
     <div class="col-sm" id="job">${element.text}</div>
     <div class="col-sm" id="time">${element.data}</div>
     <div class="col-sm" id="free">${element.free ? 'свободен/на':'занят/та'}</div>
     <button onclick ='delItem(${index})' style="width: auto;" class="btn btn-primary" display: inline; id="del_item">удалить</button>
     <button onclick ='changeItem(${index})' style="width: auto;" class="btn btn-primary" display: inline; id="change_item">редактировать</button>
+    <button onclick ='done(${index})' style="width: auto; background-color: rgb(2, 230, 2);" class="btn btn-primary" display: inline; id="done">Готово</button>
+</div>
+`
+};
+const createItemsDone = (element, index) => {
+    return `
+<div class="row ${element.done ? 'done' : ''}" id="item">
+    <div class="col-sm" id="job">${element.text}</div>
+    <div class="col-sm" id="time">${element.data}</div>
+    <div class="col-sm" id="free">${element.free ? 'свободен/на':'занят/та'}</div>
+    <button onclick ='delItem(${index})' style="width: auto;" class="btn btn-primary" display: inline; id="del_item">удалить</button>
+    <button onclick ='back(${index})' style="width: auto; background-color: rgb(2, 230, 153);" class="btn btn-primary" display: inline; id="back">Вернуть</button>
 </div>
 `
 };
 
 const addHTML = () => {
     itemBox.innerHTML = "";
+    done_box.innerHTML = "";
     if (storage.length > 0){
         storage.forEach((item, index) => {
-            itemBox.innerHTML += createItems(item, index);
+            if(item.done){
+                done_box.innerHTML += createItemsDone(item, index);
+                
+            }else{
+                itemBox.innerHTML += createItems(item, index);
+            }
+
         })
     
     }
@@ -80,11 +106,16 @@ btnRes.addEventListener('click', ()=>{
 });
 
 const delItem = (item) => {
-    storage.splice(item, 1);
+    let say = confirm('Вы уверены?');
+    if(say){
+        storage.splice(item, 1);
     console.log(item)
     updateLocal();
     addHTML();
     boxNone();
+    }else{
+        return;
+    }
 }
 
 const changeItem = (index)=>{
@@ -129,3 +160,16 @@ btnOnOff.addEventListener("click", ()=>{
     }
     truCh=moviCh;
 });
+
+const done = (index)=>{
+    storage[index].done = true;
+    updateLocal();
+    addHTML();
+    boxNone();
+}
+const back = (index)=>{
+    storage[index].done = false;
+    updateLocal();
+    addHTML();
+    boxNone();
+}
