875bb3b1d9feb266df09a8b12e8375f5c0a73033
OleGUNerz <48333829+OleGUNerz@users.noreply.github.com>
2022-10-25 16:24:49 +0200

    Refactory

--- js/main.ts
+++ js/main.ts
@@ -1,27 +1,26 @@
-const text2 = document.querySelector('#exampleDataList');
-const data = document.querySelector('#data');
-const loggle = document.getElementById('free');
-const btnSet = document.querySelector('#set');
-const btnRes = document.querySelector('#res');
-const del = document.querySelectorAll('#del')
-const itemBox = document.querySelector('.item_box');
-const checkI = document.querySelector('.item-block');
-const changeBtn = document.querySelector('.changeBtn');
-const btnOnOff = document.getElementById('changeBtn');
-const change_box = document.querySelector('.change_box');
-const jobCh = document.getElementById('jobCh');
-const timeCh = document.getElementById('timeCh');
-const saveCh = document.getElementById('saveCh');
-const snoCh = document.getElementById('noCh');
-const done_block = document.querySelector('.item-blockDone');
-const done_box = document.querySelector('.done_box');
+const form = document.querySelector('.form') as HTMLFormElement;
+const text = document.querySelector('#text') as HTMLInputElement;
+const data = document.querySelector('#date') as HTMLDataElement;
+const check = document.querySelector('#check') as HTMLInputElement;
+const popup = document.getElementById("popup") as HTMLBodyElement
+const back = document.querySelector('.back') as HTMLButtonElement;
+const save = document.querySelector('.save') as HTMLButtonElement;
+let edText = document.querySelector('#edText') as HTMLInputElement
+let edDate = document.querySelector('#edDate') as HTMLInputElement
+let chEdit = document.querySelector('#chEdit') as HTMLInputElement
 
+function clear():void{
+    text.value =""
+    data.value = ""
+    check.checked = false;
+}
 
 interface IStorage{
-    get(idTask: number): Task | null;
-    save(task: Task): void;
+    get(idTask: string): Task | null;
+    save(task: null | Task): void;
     remove(idTask:string): void;
     getAll(): Array<Task>;
+
 }
 
 class loStorage implements IStorage{
@@ -30,7 +29,7 @@ class loStorage implements IStorage{
             {
                 this.cacheKey = cacheKey
             }
-    get(idTask: number): Task | null {
+    get(idTask: string): Task | null {
         let item = localStorage.getItem(this.cacheKey)
         if(item == undefined){
             item = '[]';
@@ -38,22 +37,36 @@ class loStorage implements IStorage{
       let getted = JSON.parse(item);
       for(let i of getted){
         if(i.id == idTask){
-            return i
+            return new Task(i.id, i.text, i.data,i.free, i.done)
         }
       }
       return null
     }
-    save(task: Task): void {
+
+    save(task: null | Task): void {
         let item = localStorage.getItem(this.cacheKey)
         if(item == undefined){
             item = '[]';
         }
             let locSPac = JSON.parse(item);
-            locSPac.push(task)
-            localStorage.setItem(this.cacheKey, JSON.stringify(locSPac))
-        
-       
+        if(locSPac.length!==0) {
+            console.log("Сохраено")
+            for (let i: number = 0; i < locSPac.length; ++i) {
+                if (locSPac[i].id == task?.id) {
+                    locSPac[i] = task
+                    localStorage.setItem(this.cacheKey, JSON.stringify(locSPac))
+                    return;
+                }
+
+            }
+
+        }
+        locSPac.push(task)
+        localStorage.setItem(this.cacheKey, JSON.stringify(locSPac))
     }
+
+
+
     
     remove(idTask:string): void {
         let item = localStorage.getItem(this.cacheKey)
@@ -76,43 +89,157 @@ class loStorage implements IStorage{
             let tasks = JSON.parse(item)
             let arr = []
             for(let i of tasks){
-                let task = new Task(i.id, i.text, i.data, i.free, i.done)
+                
+                const task : Task = new Task(i.id, i.text, i.data, i.free, i.done)
                 arr.push(task)
-            
             }
             return arr
         
     }
 }
 
-
-
-
 let ne = new loStorage('tasks')
-
-
 
 class Task {
     id: string
     text: string
-    data: number
+    data: string
     free: boolean
     done: boolean
-        constructor( id: string, text1:string, data1: number, free1: boolean, done1: boolean){
+        constructor( id: string, text:string, data: string, free: boolean, done: boolean){
             this.id = id;
-            this.text = text1;
-            this.data = data1;
-            this.free = free1;
-            this.done = false;
-            
+            this.text = text;
+            this.data = data;
+            this.free = free;
+            this.done = done;
         };
-};
+}
 
 class TaskFactory{
-    create(text:string, data:number, free:boolean, done: boolean){
+    create(text:string, data:string, free:boolean, done: boolean){
         let taskId = Math.random().toString(36)
-        let creation = new Task(taskId, text, data, free, done);
-        return creation;
+        return new Task(taskId, text, data, free, done);
     };
-};
+}
 let mo = new TaskFactory;
+
+
+form.addEventListener('submit', (e:Event)=>{
+    e.preventDefault();
+    if(text.value){
+        if(!data.value){
+            const dat = new Date
+            const d = `${dat.getFullYear()}-${dat.getMonth() + 1}-${dat.getDate()}`
+            data.value = d.toString()
+        }
+        const t = mo.create(text.value,data.value,check.checked,false)
+    ne.save(t)
+        clear()
+    renderTask()
+    }else{
+        alert("введи задачу")
+    }
+    
+})
+
+back.addEventListener("click", ()=>{
+    if(popup){
+        popup.classList.add("none")
+    }
+
+})
+
+function renderTask() {
+    let template:string | null = document.getElementById('template')!.innerHTML;
+    document.getElementById('target')!.innerHTML = Mustache.render(template, {Task:(ne.getAll())});
+    let trFa = document.querySelectorAll('td')
+    for(let i of trFa){
+        if(i.innerHTML == 'false'){
+            i.innerHTML = "Занят/та"
+        }else if(i.innerHTML == 'true'){
+            i.innerHTML = "Свободен/а"
+        }
+    }
+    const btnDel = document.querySelectorAll('.dell')
+    const btnDone = document.querySelectorAll('.done')
+    const btnEdit = document.querySelectorAll('.edit')
+
+    for(let i of btnDel){
+        i.addEventListener('click', (e)=>{
+            if(confirm("Вы уверены?")){
+                ne.remove(e.target.id)
+                renderTask()
+            }
+        })
+    }
+    for(let i of btnDone){
+        i.addEventListener('click', (e)=>{
+            doneTask(e.target.id)
+            renderTask()
+        })
+    }
+    for(let i of btnEdit){
+        i.addEventListener('click', (e)=>{
+            editTask(e.target.id)
+            saves(e.target.id)
+            renderTask()
+        })
+    }
+}
+
+renderTask()
+
+
+function doneTask(id:string):void{
+    let locDone : Task | null = ne.get(id);
+    if(locDone){
+        locDone.done ? locDone.done = false : locDone.done = true
+    }
+    ne.save(locDone)
+    renderTask()
+}
+
+function saves(id:string) {
+    const p = new Promise(function (resolve, reject){
+        save.addEventListener("click", ()=>{
+            resolve(id)
+        })
+        back.addEventListener("click",()=>{
+            reject(id)
+            return
+        })
+    })
+
+    p.then((id:string):void=>{
+        let locTask : Task | null = ne.get(id);
+            locTask?.text = edText.value;
+            locTask?.data = edDate.value;
+            locTask?.free = chEdit.checked;
+            if(edText.value !==""){
+                ne.save(locTask)
+                if(popup){
+                    popup.classList.add("none")
+                }
+                renderTask()
+            }else{
+                if(confirm("Вы хоте удалить пустую таску?")){
+                    ne.remove(id)
+                    popup.classList.add("none")
+                    renderTask()
+                }else{
+                    saves(id)
+                }
+            }
+
+    }).catch((id)=>console.error("Отмена редактирования таски: -", id))
+}
+
+function editTask(id:string) {
+    let locTask : Task | null = ne.get(id);
+    if(popup){
+        popup.classList.remove("none")
+    }
+        edText.value = locTask?.text;
+        edDate.value = locTask?.data;
+        chEdit.checked = locTask?.free;
+}
